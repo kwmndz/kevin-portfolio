e44bcc8268a3537b357464c232e8213ee2378c5b
kwmndz <kwmndz@umich.edu>
2025-01-15 00:07:48 -0500

    more prod changes

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/kevinmendez',
+  assetPrefix: '/kevinmendez',
   images: {
     unoptimized: true,
   },
